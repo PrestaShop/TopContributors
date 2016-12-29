b108be70ffc99698b5cd5c1baeb1c170b2b959a2
Mickaël Andrieu <andrieu.travail@gmail.com>
2016-12-29 17:53:28 +0100

    Introduced TopContributors block (WIP)

--- src/components/ui/Arrow.vue
+++ src/components/ui/Arrow.vue
@@ -1,13 +1,19 @@
 <template>
     <div id="arrow">
-        <svg className="mask" viewBox="0 0 10 5" preserveAspectRatio="none">
+        <svg class="mask" viewBox="0 0 10 5" preserveAspectRatio="none">
             <g stroke="none" fill="#fff">
                 <polygon points="-1 -1 -1 5 5 0 11 5 11 -1" />
             </g>
         </svg>
-        <div className="words-count text-md-center">
-            <div className="dot" />
-            {{ counts }}
+        <div class="words-count text-md-center">
+            <div class="dot" />
+            <div v-for="(count, index) in counts">
+                <div :rank="index">
+                <div class="count">{{ count }}</div>
+                <div class="words">contributions</div>
+                <div class="dot" />
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -17,25 +23,7 @@
 
 <script>
     export default{
-        props(){
-            return {
-                message
-            }
-        },
-        data(){
-            console.log(this.message)
-            return {
-                counts: []
-                /*counts: this.counts.forEach((value, i) => {
-                  counts.push(
-                    '<div key='+i+'>'+
-                      '<div className="count">'+value+'</div>'+
-                      '<div className="words">words</div>'+
-                      '<div className="dot" />'+
-                    '</div>'
-                  )
-                })*/
-            }
-        }
+        name: 'Arrow',
+        props: ['counts'],
     }
 </script>
