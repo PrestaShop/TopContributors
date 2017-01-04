d6d813c6233db8c5c6b890b3805a45a900eda0ef
Mickaël Andrieu <andrieu.travail@gmail.com>
2017-01-04 15:14:15 +0100

    Added pagination block

--- src/components/Beginners.vue
+++ src/components/Beginners.vue
@@ -2,9 +2,9 @@
     <div>
         <section-header category="beginners" title="Beginners (1 to 5 contrib.)" v-bind:stars="1"></section-header>
         <ul id="newbie-contributors-list" class="col-md-10">
-            <avatar-item v-for="contributor in contributors" v-bind:contributor="contributor" category="rank-beginners"></avatar-item>
+            <avatar-item v-for="contributor in displayedContributors" v-bind:contributor="contributor" category="rank-beginners"></avatar-item>
         </ul>
-        <pagination pages="pages"></pagination>
+        <pagination pages="pages" v-on:select-page="displayBeginners"></pagination>
     </div>
 </template>
 
@@ -15,18 +15,34 @@
 
     export default{
         name: 'Beginners',
+        data(){
+            return{
+                pageIndex: 1
+            }
+        },
         props: [
             'contributors',
         ],
         computed: {
+            displayedContributors: function() {
+                let start = 50*this.pageIndex;
+                let end = 50*(this.pageIndex+1);
+
+                return this.contributors.slice(start, end);
+            },
             pages: function() {
-                return [Array(Math.ceil(this.contributors.length % 50)).keys()];
+                return [Array(Math.ceil(this.contributors.length / 50)).keys()];
             }
         },
         components: {
             'section-header': TitleOfSection,
             'avatar-item': AvatarItem,
             'pagination': Pagination
+        },
+        methods: {
+            displayBeginners: function (pageIndex) {
+                this.pageIndex = pageIndex;
+            }
         }
     }
 </script>
--- src/components/ui/Pagination.vue
+++ src/components/ui/Pagination.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <span v-for="(page, index) in pages"><a href="#"> {{ index +1 }}</a></span>
+    <div class="text-md-center beginners-pagination">
+        <span v-for="(page, index) in pages"><a v-on:click.prevent="select(index)" href="#">{{ index +1 }}</a></span>
     </div>
 </template>
 <script>
@@ -9,6 +9,11 @@
         props: ['pages'],
         computed: function (page) {
             return '#'+page;
+        },
+        methods: {
+            select: function(index) {
+                this.$emit('select-page', index +1);
+            }
         }
     }
 </script>
