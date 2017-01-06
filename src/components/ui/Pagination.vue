97c9be3aec93ad3b085c59394aff5750a279bb38
Mickaël Andrieu <andrieu.travail@gmail.com>
2017-01-06 14:14:49 +0100

    Added pagination active system

--- src/components/ui/Pagination.vue
+++ src/components/ui/Pagination.vue
@@ -1,18 +1,29 @@
 <template>
     <div class="text-md-center beginners-pagination">
-        <span v-for="(page, index) in pages"><a v-on:click.prevent="select(index)" href="#">{{ index +1 }}</a></span>
+        <span v-for="(page, index) in pages"><a v-on:click.prevent="select(index)" href="#" :class="isActive(index)">{{ index +1 }}</a></span>
     </div>
 </template>
 <script>
     export default{
         name: 'pagination',
         props: ['pages'],
-        computed: function (page) {
-            return '#'+page;
+        data(){
+            return {
+                activePage: 1
+            }
         },
         methods: {
+            isActive: function(index) {
+                if((index +1) === this.activePage) {
+                    return 'active';
+                }
+
+                return '';
+            },
             select: function(index) {
-                this.$emit('select-page', index +1);
+                let page = index + 1;
+                this.activePage = page;
+                this.$emit('select-page', page);
             }
         }
     }
