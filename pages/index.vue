2b943615bb6e7b244ab8f2700374ea4723b3b8db
Valentin Szczupak <valentin.szczupak@prestashop.com>
2020-11-27 17:23:46 +0100

    Refacto to Nuxt + TypeScript

---
+++ pages/index.vue
@@ -0,0 +1,109 @@
+<template>
+  <div>
+    <header>
+      <div class="gradient">
+        <div class="container">
+          <div class="row justify-content-md-center">
+            <div class="col col-md-8 text-md-center">
+              <h1>Top contribution achievements</h1>
+              <p class="white-text">
+                Welcome!<br /><br />
+                This page is dedicated to the code contributors building
+                PrestaShop every day. Check below to see how much you helped the
+                community, thanks to your involvement in open source.
+              </p>
+              <div>
+                <a
+                  href="https://github.com/PrestaShop/PrestaShop"
+                  class="btn btn-primary"
+                  >Contribute now</a
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="container">
+      <top-contributors />
+    </div>
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-4">
+            <div class="white-title">JOIN US ON GITHUB</div>
+            <div class="white-text">
+              To join the community and help the project, go to GitHub's
+              PrestaShop page and register.
+            </div>
+            <a
+              href="https://github.com/PrestaShop/PrestaShop"
+              class="btn btn-outline-info"
+              >Contribute now</a
+            >
+          </div>
+          <div class="col-4">
+            <div class="white-title">CONTRIBUTOR'S GUIDE</div>
+            <div class="white-text">
+              Follow our guide to discover the best practices and procedure to
+              follow when you contribute to PrestaShop.
+            </div>
+            <a
+              href="https://devdocs.prestashop.com/1.7/contribute/"
+              class="btn btn-outline-info large-padding"
+              >Read the docs</a
+            >
+          </div>
+          <div class="col-4">
+            <div class="white-title">DISCOVER PRESTASHOP 1.7</div>
+            <div class="white-text">
+              Check the latest additions and the more significant improvements
+              of the 1.7 version, the next major version of PrestaShop.
+            </div>
+            <a
+              href="https://www.prestashop.com/en/1.7"
+              class="btn btn-outline-info large-padding"
+              >Discover</a
+            >
+          </div>
+        </div>
+
+        <div class="row">
+          <div id="badge" class="col-md-12 text-md-center">
+            <img src="../static/img/badge.png" />
+            <hr />
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <a class="build-link" href="https://build.prestashop.com"
+              >Follow the blog of our Product &amp; Dev team</a
+            >
+          </div>
+          <div class="col-md-6 text-md-right share">
+            <span>Follow us</span>
+            <a href="https://www.facebook.com/prestashop"
+              ><img src="../static/img/facebook.png"
+            /></a>
+            <a href="https://twitter.com/prestashop"
+              ><img src="../static/img/twitter.png"
+            /></a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import TopContributors from '../components/TopContributors.vue'
+
+export default Vue.extend({
+  components: {
+    TopContributors
+  }
+})
+</script>
+
+<style lang="scss" src="../assets/style/theme.scss"></style>
