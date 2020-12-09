82350274a541baf9e1a1cfa0a97166ddd37229cf
NeOMakinG <valentin.szczupak@prestashop.com>
2020-12-09 14:45:31 +0100

    Remote responsive meta temporary

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,6 @@ export default {
     title: 'PrestaShop Top Contributors',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
