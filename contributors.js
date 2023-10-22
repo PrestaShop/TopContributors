a4ef26af4780944ab9a9efb2e0b0d21c1787c81c
matthieu-rolland <matthieu-rolland@users.noreply.github.com>
2023-10-22 06:08:30 +0000

    deploy: 44c3d5366ccad67f88a02abf6509c18714b6cf33

--- contributors.js
+++ contributors.js
@@ -10,7 +10,7 @@
         "location": "Rennes, France",
         "bio": null,
         "email_domain": "gmail.com",
-        "contributions": 6031,
+        "contributions": 6032,
         "repositories": {
             "PrestaShop\/PrestaShop": 3181,
             "PrestaShop\/PrestaShop-modules": 31,
@@ -108,7 +108,7 @@
             "PrestaShop\/stylelint-config": 7,
             "PrestaShop\/example-modules": 6,
             "PrestaShop\/ps_qualityassurance": 1,
-            "PrestaShop\/test-scenarios": 433,
+            "PrestaShop\/test-scenarios": 434,
             "PrestaShop\/user-documentation-it": 2,
             "PrestaShop\/user-documentation-en": 1,
             "PrestaShop\/hummingbird": 6,
@@ -146,12 +146,12 @@
                 }
             },
             "documentation": {
-                "total": 601,
+                "total": 602,
                 "repositories": {
                     "PrestaShop\/docs": 143,
                     "PrestaShop\/user-documentation-en": 1,
                     "PrestaShop\/user-documentation-it": 2,
-                    "PrestaShop\/test-scenarios": 433,
+                    "PrestaShop\/test-scenarios": 434,
                     "PrestaShop\/example-modules": 6,
                     "PrestaShop\/open-source": 16
                 }
@@ -39611,5 +39611,5 @@
             }
         }
     },
-    "updatedAt": "2023-10-21 06:06:01"
+    "updatedAt": "2023-10-22 06:08:29"
 }
